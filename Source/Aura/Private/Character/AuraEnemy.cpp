797fa6f48243db6f6dc544121c0b871b027810b9
kubaz <zurawikjakub@gmail.com>
2024-12-15 21:15:08 +0100

    Add Movement
    - Add EnhacedInput
    - Add EnemyInterface
    - Highligh Enemy

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -1,5 +1,20 @@
-
-
-
 #include "Character/AuraEnemy.h"
 
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	GetMesh()->SetCustomDepthStencilValue(250.f);
+	Weapon->SetCustomDepthStencilValue(250.f);
+}
+
+void AAuraEnemy::HighLightActor()
+{
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
+}
+
+void AAuraEnemy::UnHighLightActor()
+{
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+}
