bda112957388c891eeb72dd8bba45a8d26d5e6bb
kubaz <zurawikjakub@gmail.com>
2025-06-01 12:05:56 +0200

    Player Level and Combat Interface

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -13,6 +13,8 @@ public:
 	AAuraCharacter();
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
+
+	virtual int32 GetPlayerLevel() override;
 	
 	
 protected:
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -22,10 +22,15 @@ public:
 	virtual void HighLightActor() override;
 	virtual void UnHighLightActor() override;
 
+	virtual int32 GetPlayerLevel() override;
+
 	UPROPERTY(BlueprintReadOnly)
 	bool bIsHighlighted{ false };
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
 };
